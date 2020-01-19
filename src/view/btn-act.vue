13bc1f18c03e62c5872e02030516bc8ce1dd978c
xachary <179740385@qq.com>
2020-01-19 09:45:11 +0800

    btn-loading and btn-act

--- src/view/btn-act.vue
+++ src/view/btn-act.vue
@@ -1,52 +1,48 @@
 <template>
-    <div class="sample">
-        <label v-xy-btn-act:style.scale>scale</label>
-        <label v-xy-btn-act:style.color="'rgba(0, 0, 255, 0.2)'">color</label>
-        <label
-            style="background-color:yellow"
-            v-xy-btn-act:style.grayscale
-        >grayscale</label>
-        <label v-xy-btn-act:style.wave="'rgba(255, 0, 0, 0.5)'">wave</label>
-        <label
-            v-xy-btn-act:style.scale
-            disabled
-        >disabled</label>
-    </div>
+  <div class="sample">
+    <button v-xy-btn-act:style.scale>scale</button>
+    <button v-xy-btn-act:style.color="'rgba(0, 0, 255, 0.2)'">color</button>
+    <button style="background-color:yellow"
+            v-xy-btn-act:style.grayscale>grayscale</button>
+    <button v-xy-btn-act:style.wave="'rgba(255, 0, 0, 0.5)'">wave</button>
+    <button v-xy-btn-act:style.scale
+            disabled>disabled</button>
+  </div>
 </template>
 
 <script>
-export default {
-    components: {
-    },
+  export default {
+    components: {},
     data() {
-        return {
-            status: ''
-        }
+      return {
+        status: ''
+      }
     },
     methods: {
-        onBottom() {
-            alert('bottom')
-        }
+      onBottom() {
+        alert('bottom')
+      }
     },
-    mounted: function () {
-    }
-}
+    mounted: function() {}
+  }
 </script>
 
 <style lang="scss" scoped>
-.sample {
-  label {
-    padding: 1em 2em;
-    border: 1px solid #333;
-    margin: 1em;
-    display: block;
-    text-align: center;
+  .sample {
+    display: flex;
+    flex-direction: column;
+    button {
+      padding: 1em 2em;
+      border: 1px solid #333;
+      margin: 1em;
+      display: block;
+      text-align: center;
+    }
+    [xy-btn-act][disabled] {
+      filter: grayscale(1);
+      background-color: rgba(0, 0, 0, 0.1);
+      color: rgba(0, 0, 0, 0.2);
+      border-color: rgba(0, 0, 0, 0.2);
+    }
   }
-  [xy-btn-act][disabled] {
-    filter: grayscale(1);
-    background-color: rgba(0, 0, 0, 0.1);
-    color: rgba(0, 0, 0, 0.2);
-    border-color: rgba(0, 0, 0, 0.2);
-  }
-}
 </style>
